fc3ebff18e1a58a54466691b41b793a66158c14e
wlghditkfkdgo <yunseon241@icloud.com>
2026-03-03 15:01:18 +0900

    classification, anomalydetection: add script files

---
+++ anomaly_detection/scripts/PSM.sh
@@ -0,0 +1,36 @@
+data=PSM
+patch_size=16
+seq_len=100
+c_out=25
+root_path=/home/yschoi/CLS_spiking_transformer/Bio-inspired-Spiking-Memory-Transformer-for-time-series-representation-learning/anomaly_detection/dataset/${data}
+
+for gating in original ablation
+do
+for lr in 0.0005
+do
+python3 ./train.py \
+    --gating ${gating} \
+    --no-bias \
+    --scheduler reduce \
+    -s \
+    --test \
+    -nd 2 \
+    -e 100 \
+    --warm_up_epoch 0 \
+    -bs 256 \
+    -emb 64 \
+    -nh 8 \
+    -lr ${lr} \
+    --alpha 1 \
+    --time_layers 1 \
+    --mlp_ratios 2 \
+    --data ${data} \
+    --pred_len 0 \
+    --patch_size ${patch_size} \
+    --root_path ${root_path} \
+    --seq_len ${seq_len} \
+    --c_out ${c_out} \
+    --anomaly_ratio 1 \
+    --features M 
+done
+done
